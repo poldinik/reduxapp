d32501e9c200283884d07f9def0348aa9b346049
poldinik <lorenzo.vannucchi1993@gmail.com>
2020-08-19 17:53:47 +0200

    rimuove commenti

--- reduxapp-client/src/main/java/studio/volare/reduxapp/gwtredux/StoreProvider.java
+++ reduxapp-client/src/main/java/studio/volare/reduxapp/gwtredux/StoreProvider.java
@@ -6,7 +6,6 @@ import studio.volare.reduxapp.redux.Store;
 public class StoreProvider<S> {
     final Store<S> store;
     final Widget widget;
-
     public StoreProvider(Store<S> store, Widget widget) {
         this.store = store;
         this.widget = widget;
